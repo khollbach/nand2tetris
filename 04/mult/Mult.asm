0331b9c331ca86bbac46bfbd02dfc4463394cb46
Kevan Hollbach <khollbach@gmail.com>
2023-04-19 19:59:32 -0700

    04: Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,33 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// We want to compute x * y, which is the same as (y + ... + y) for x copies of
+// y.
+//
+// So we'll loop x times, and add y to an accumulator once per loop iteration.
+
+(START)
+    @R2
+    M=0
+
+(LOOP)
+    // If R0 equals 0, jump to end.
+    @R0
+    D=M
+    @END
+    D;JEQ
+
+    // R2 += R1
+    @R1
+    D=M
+    @R2
+    M=D+M
+
+    // R0 -= 1
+    @R0
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(END)
