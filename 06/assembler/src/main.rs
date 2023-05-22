41ae7f3d4aba3bce42a6ff81fa852ae6e8019ccc
Kevan Hollbach <khollbach@gmail.com>
2023-05-22 13:35:38 -0700

    06: open input and output files

--- 06/assembler/src/main.rs
+++ 06/assembler/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    ffi::OsStr,
+    fs::File,
+    io::{prelude::*, BufReader, SeekFrom},
+    path::Path,
+};
+
+use anyhow::{ensure, Context, Result};
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    let (mut in_file, _out_file) = open_files()?;
+
+    // first pass
+    for line in BufReader::new(&mut in_file).lines() {
+        dbg!(line?);
+    }
+
+    // second pass
+    in_file.seek(SeekFrom::Start(0))?;
+    for line in BufReader::new(&mut in_file).lines() {
+        dbg!(line?);
+    }
+
+    Ok(())
+}
+
+fn open_files() -> Result<(File, File)> {
+    let (_prog_name, path) = env::args().collect_tuple().with_context(|| {
+        let n = env::args().count().saturating_sub(1);
+        format!("expected one argument, got {n}")
+    })?;
+    let path: &Path = path.as_ref();
+
+    let ext = path.extension().and_then(OsStr::to_str);
+    ensure!(
+        ext == Some("asm"),
+        "must have .asm extension: {}",
+        path.display()
+    );
+
+    let in_file =
+        File::open(path).with_context(|| format!("couldn't open file {}", path.display()))?;
+
+    // These should succeed, since `.extension()` suceeded.
+    let dir = path.parent().unwrap();
+    let mut out_name = path.file_stem().unwrap().to_owned();
+    out_name.push(".hack");
+
+    let out_path = dir.join(out_name);
+    let out_file = File::create(&out_path)
+        .with_context(|| format!("couldn't create file {}", out_path.display()))?;
+
+    Ok((in_file, out_file))
 }
