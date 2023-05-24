3735e03d037b10e301abdd633e6899624c75affd
Kevan Hollbach <khollbach@gmail.com>
2023-05-23 21:32:11 -0700

    06: remove the output file if translation fails

--- 06/assembler/src/main.rs
+++ 06/assembler/src/main.rs
@@ -3,9 +3,9 @@ mod instr;
 use std::{
     env,
     ffi::OsStr,
-    fs::File,
+    fs::{self, File},
     io::{prelude::*, BufReader},
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use anyhow::{ensure, Context, Result};
@@ -14,9 +14,50 @@ use itertools::Itertools;
 use crate::instr::Instr;
 
 fn main() -> Result<()> {
-    let (mut in_file, mut out_file) = open_files()?;
+    let (_prog_name, in_path) = env::args().collect_tuple().with_context(|| {
+        let n = env::args().count().saturating_sub(1);
+        format!("expected one argument, got {n}")
+    })?;
+    let out_path = out_path(&in_path)?;
 
-    let lines = BufReader::new(&mut in_file)
+    let in_file = File::open(&in_path).with_context(|| format!("couldn't open file {in_path}"))?;
+    let out_file = File::create(&out_path)
+        .with_context(|| format!("couldn't create file {}", out_path.display()))?;
+
+    let result = translate(in_file, out_file);
+
+    // If unsucessful, clean up the output file.
+    if result.is_err() {
+        if let Err(e) = fs::remove_file(&out_path) {
+            eprintln!("failed to clean up output file {}: {e}", out_path.display());
+        }
+        result?;
+    };
+
+    Ok(())
+}
+
+fn out_path(path: impl AsRef<Path>) -> Result<PathBuf> {
+    let path = path.as_ref();
+
+    let ext = path.extension().and_then(OsStr::to_str);
+    ensure!(
+        ext == Some("asm"),
+        "must have .asm extension: {}",
+        path.display()
+    );
+
+    // These should succeed, since `.extension()` suceeded.
+    let dir = path.parent().unwrap();
+    let mut out_name = path.file_stem().unwrap().to_owned();
+    out_name.push(".hack");
+
+    let out_path = dir.join(out_name);
+    Ok(out_path)
+}
+
+fn translate(in_file: File, mut out_file: File) -> Result<()> {
+    let lines = BufReader::new(in_file)
         .lines()
         .map(|r| r.map_err(Into::into));
 
@@ -26,42 +67,7 @@ fn main() -> Result<()> {
         writeln!(out_file, "{code:0>16b}")?;
     }
 
-    // // second pass
-    // in_file.seek(SeekFrom::Start(0))?;
-    // for line in BufReader::new(in_file).lines() {
-    //     dbg!(line?);
-    // }
-
     Ok(())
-}
-
-fn open_files() -> Result<(File, File)> {
-    let (_prog_name, path) = env::args().collect_tuple().with_context(|| {
-        let n = env::args().count().saturating_sub(1);
-        format!("expected one argument, got {n}")
-    })?;
-    let path: &Path = path.as_ref();
-
-    let ext = path.extension().and_then(OsStr::to_str);
-    ensure!(
-        ext == Some("asm"),
-        "must have .asm extension: {}",
-        path.display()
-    );
-
-    let in_file =
-        File::open(path).with_context(|| format!("couldn't open file {}", path.display()))?;
-
-    // These should succeed, since `.extension()` suceeded.
-    let dir = path.parent().unwrap();
-    let mut out_name = path.file_stem().unwrap().to_owned();
-    out_name.push(".hack");
-
-    let out_path = dir.join(out_name);
-    let out_file = File::create(&out_path)
-        .with_context(|| format!("couldn't create file {}", out_path.display()))?;
-
-    Ok((in_file, out_file))
 }
 
 /// Remove comments and blank lines.
